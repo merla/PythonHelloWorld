345dd5e981e536300d8038f4bf67bb231f24c6de
merla <prathyusharani9@gmail.com>
2018-05-02 13:10:26 -0400

    Add files via upload
    added file

---
+++ lambda1.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    #print("Received event: " + json.dumps(event, indent=2))
+    
+   
+    
+    return "Hi Prathyu"  # Echo back the first key value
+    #raise Exception('Something went wrong')
