f53d7d7a1825a2a5f6870e198cc11f2c534e31bc
merla <prathyusharani9@gmail.com>
2018-05-03 11:59:20 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -10,5 +10,5 @@ def lambda_handler(event, context):
     
    
     
-    return "Got it from lambda"  # Echo back the first key value
+    return "Try it from lambda"  # Echo back the first key value
     #raise Exception('Something went wrong')
